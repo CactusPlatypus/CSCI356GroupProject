5d2192295065638d28abf95fd731469489bcc2f6
Jack-Is-2048 <101245336+Jack-Is-2048@users.noreply.github.com>
2022-09-05 17:25:40 +1000

    Added Infinite Road Generation

---
+++ Group Project/Assets/Scripts/TempCharController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TempCharController : MonoBehaviour
+{
+    public float movementSpeed = 10f;
+    public SpawnManager spawnManager;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float hMovement = Input.GetAxis("Horizontal") * movementSpeed / 2f;
+        float vMovement = Input.GetAxis("Vertical") * movementSpeed;
+        transform.Translate(new Vector3(hMovement, 0, vMovement) * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("SpawnTrigger"))
+        {
+            spawnManager.SpawnTriggerEntered();
+        }
+    }
+}
