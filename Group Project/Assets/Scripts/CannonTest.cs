650f791df8cecbc5106bea1ef50fef1bf0f5ccef
Tom Jedi <65552757+tomhobeika@users.noreply.github.com>
2022-09-06 14:06:48 +1000

    Starting cannon

---
+++ Group Project/Assets/Scripts/CannonTest.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CannonTest : MonoBehaviour
+{
+    private Transform shotPoint;
+    public GameObject KalebRagdoll; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        shotPoint = this.gameObject.transform.GetChild(0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space")){
+            Instantiate(KalebRagdoll, shotPoint.position, Quaternion.identity);
+        }
+    }
+}
