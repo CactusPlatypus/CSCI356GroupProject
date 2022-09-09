fd4241e4e5e11ac6c8d46a5968a8721738fdd265
Tom Jedi <65552757+tomhobeika@users.noreply.github.com>
2022-09-09 15:22:54 +1000

    Epic ragdoll cannon + Unity standard assets

--- Group Project/Assets/Scripts/CannonTest.cs
+++ Group Project/Assets/Scripts/CannonTest.cs
@@ -1,22 +1,42 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CannonTest : MonoBehaviour
 {
     private Transform shotPoint;
-    public GameObject KalebRagdoll; 
-    // Start is called before the first frame update
+    public GameObject KalebRagdoll;
+    public GameObject explosion; 
+    Vector3 m_NewForce;
+    public float launchForce = 50.0f;
+    
     void Start()
     {
         shotPoint = this.gameObject.transform.GetChild(0);
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown("space")){
-            Instantiate(KalebRagdoll, shotPoint.position, Quaternion.identity);
+            Shoot();
         }
+    }
+    // Method currently called by spacebar, can be called by anything
+    void Shoot(){
+        gameObject.GetComponent<AudioSource>().Play();
+
+        GameObject kaleb = Instantiate(KalebRagdoll, shotPoint.position, Quaternion.identity, this.gameObject.transform);
+        GameObject explosionI = Instantiate(explosion, shotPoint.position, Quaternion.identity, this.gameObject.transform);
+
+        StartCoroutine(DestroyExplosion(4f, explosionI));
+        
+        // Takes forward direction of cannon, and applies it to Kaleb
+        m_NewForce = new Vector3(this.gameObject.transform.forward.x*launchForce, this.gameObject.transform.forward.y*launchForce, this.gameObject.transform.forward.z*launchForce);
+        // This line is fucked because it is grabbing Kaleb's spine's rigidbody, not his overall one
+        kaleb.transform.GetChild(1).transform.GetChild(0).transform.GetChild(0).transform.GetChild(2).GetComponent<Rigidbody>().AddForce(m_NewForce, ForceMode.Impulse);
+    }
+    IEnumerator DestroyExplosion(float seconds, GameObject explosionI){
+        yield return new WaitForSeconds(seconds);
+        Destroy(explosionI);
     }
 }
