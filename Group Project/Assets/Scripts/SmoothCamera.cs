793e19862f2ee5c2592eb4d18514d0ae38e11128
MysteryPancake <MysteryPancake@users.noreply.github.com>
2022-09-11 04:23:46 +1000

    fov change too jank

--- Group Project/Assets/Scripts/SmoothCamera.cs
+++ Group Project/Assets/Scripts/SmoothCamera.cs
@@ -5,21 +5,14 @@ public class SmoothCamera : MonoBehaviour
 {
     public Transform target;
 
-    private Camera cam;
     private Vector3 offset = new Vector3(0f, 3f, 4f);
     private float positionSharpness = 5f;
     private float rotationSharpness = 5f;
-
-    private void Start()
-    {
-        cam = GetComponent<Camera>();
-    }
 
     void Update()
     {
         Vector3 targetPosition = target.TransformPoint(offset);
         transform.position = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime * positionSharpness);
         transform.rotation = Quaternion.Lerp(transform.rotation, target.rotation, Time.deltaTime * rotationSharpness);
-        cam.fieldOfView = 60f + Vector3.Distance(transform.position, targetPosition) * 2f;
     }
 }
