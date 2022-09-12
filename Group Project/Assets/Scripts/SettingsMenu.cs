f933d478f767a0a3293dcd2ddad4a97cb3905a15
CactusPlatypus <70307680+CactusPlatypus@users.noreply.github.com>
2022-09-13 09:32:29 +1000

    settings menu with volume sliders added

--- Group Project/Assets/Scripts/SettingsMenu.cs
+++ Group Project/Assets/Scripts/SettingsMenu.cs
@@ -6,10 +6,45 @@ public class SettingsMenu : MonoBehaviour
 {
 
     public AudioMixer audioMixer;
+    public GameObject mainMenu;
+    public GameObject settingsMenu;
 
-    public void setVolume(float volume)
+
+    public void setMasterVolume(float volume)
     {
         audioMixer.SetFloat("MasterVolume", volume);
+    }
+
+    public void setMusicVolume(float volume)
+    {
+        audioMixer.SetFloat("MusicVolume", volume);
+    }
+
+    public void setSFXVolume(float volume)
+    {
+        audioMixer.SetFloat("SFXVolume", volume);
+    }
+
+    public void showMainMneu()
+    {
+        mainMenu.SetActive(true);
+        hideSettings();
+    }
+
+    public void hideMainMenu()
+    {
+        mainMenu.SetActive(false);
+    }
+
+    public void showSettings()
+    {
+        settingsMenu.SetActive(true);
+        hideMainMenu();
+    }
+
+    public void hideSettings()
+    {
+        settingsMenu.SetActive(false);
     }
 
 }
