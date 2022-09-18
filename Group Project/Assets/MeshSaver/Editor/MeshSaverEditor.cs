322387e8c950873e6ba86181deac9bb22ceca84d
MysteryPancake <MysteryPancake@users.noreply.github.com>
2022-09-19 01:51:47 +1000

    spline roads

--- Group Project/Assets/MeshSaver/Editor/MeshSaverEditor.cs
+++ Group Project/Assets/MeshSaver/Editor/MeshSaverEditor.cs
@@ -6,24 +6,18 @@ using System.Collections.Generic;
 
 public static class MeshSaverEditor {
 
-	[MenuItem("CONTEXT/MeshFilter/Save Mesh...")]
+	[MenuItem("CONTEXT/MeshFilter/Bake Path...")]
 	public static void SaveMeshInPlace (MenuCommand menuCommand) {
 		MeshFilter mf = menuCommand.context as MeshFilter;
 		Mesh m = mf.sharedMesh;
 		SaveMesh(m, m.name, false, true);
 	}
 
-	[MenuItem("CONTEXT/MeshFilter/Save Mesh As New Instance...")]
-	public static void SaveMeshNewInstanceItem (MenuCommand menuCommand) {
-		MeshFilter mf = menuCommand.context as MeshFilter;
-		Mesh m = mf.sharedMesh;
-		SaveMesh(m, m.name, true, true);
-	}
-
 	public static void SaveMesh (Mesh mesh, string name, bool makeNewInstance, bool optimizeMesh) {
-		string path = EditorUtility.SaveFilePanel("Save Separate Mesh Asset", "Assets/", name, "asset");
+		string path = EditorUtility.SaveFilePanel("Save Separate Mesh Asset", "Assets/Prefabs/BakedRoads", name, "asset");
+
 		if (string.IsNullOrEmpty(path)) return;
-        
+
 		path = FileUtil.GetProjectRelativePath(path);
 
 		Mesh meshToSave = (makeNewInstance) ? Object.Instantiate(mesh) as Mesh : mesh;
