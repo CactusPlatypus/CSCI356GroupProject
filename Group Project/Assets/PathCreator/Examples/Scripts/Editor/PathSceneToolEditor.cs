7ef956897e191d97ea108ce4372c29dd9134a6fb
MysteryPancake <MysteryPancake@users.noreply.github.com>
2022-09-19 03:08:53 +1000

    update pathcreator to add bake button

--- Group Project/Assets/PathCreator/Examples/Scripts/Editor/PathSceneToolEditor.cs
+++ Group Project/Assets/PathCreator/Examples/Scripts/Editor/PathSceneToolEditor.cs
@@ -1,6 +1,8 @@
 ﻿using UnityEngine;
 using UnityEditor;
 using PathCreation;
+using System.IO;
+using UnityEditor.SceneManagement;
 
 namespace PathCreation.Examples
 {
@@ -41,6 +43,66 @@ namespace PathCreation.Examples
                 }
             }
 
+            GUILayout.BeginHorizontal();
+
+            if (GUILayout.Button("Bake Road"))
+            {
+                if (TryFindPathCreator())
+                {
+                    // Get road mesh from path tool, assuming Road Mesh Creator script was added
+                    Mesh mesh = pathTool.GetMesh();
+                    MeshUtility.Optimize(mesh);
+                    
+                    // Create folder for storing baked road prefabs
+                    if (!Directory.Exists("Assets/Prefabs/BakedRoads"))
+                    {
+                        AssetDatabase.CreateFolder("Assets/Prefabs", "BakedRoads");
+                    }
+
+                    // Save mesh into folder
+                    string name = pathTool.gameObject.name;
+                    AssetDatabase.CreateAsset(mesh, "Assets/Prefabs/BakedRoads/" + name + ".asset");
+                    AssetDatabase.SaveAssets();
+                    
+                    // Copy mesh rendering stuff to a new game object
+                    GameObject copy = Instantiate(pathTool.GetMeshHolder(), pathTool.gameObject.transform.parent);
+                    copy.name = "Baked " + name;
+                    copy.AddComponent<MeshCollider>();
+
+                    // Hide original mesh in case we want to edit it later
+                    pathTool.gameObject.SetActive(false);
+
+                    // Force prefab to autosave
+                    EditorSceneManager.MarkSceneDirty(pathTool.gameObject.scene);
+                }
+            }
+
+            if (GUILayout.Button("Unbake Road"))
+            {
+                if (TryFindPathCreator())
+                {
+                    // Delete saved mesh
+                    string name = pathTool.gameObject.name;
+                    string path = "Assets/Prefabs/BakedRoads/" + name + ".asset";
+                    if (File.Exists(path))
+                    {
+                        AssetDatabase.DeleteAsset(path);
+                        AssetDatabase.SaveAssets();
+                    }
+
+                    // Delete baked spline
+                    Transform baked = pathTool.gameObject.transform.parent.Find("Baked " + name);
+                    DestroyImmediate(baked.gameObject);
+
+                    // Show original mesh
+                    pathTool.gameObject.SetActive(true);
+
+                    // Force prefab to autosave
+                    EditorSceneManager.MarkSceneDirty(pathTool.gameObject.scene);
+                }
+            }
+
+            GUILayout.EndHorizontal();
         }
 
 
