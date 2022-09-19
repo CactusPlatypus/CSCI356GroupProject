4befcac5c5ced1064a9a6f297ccd32d4d15b05b5
MysteryPancake <MysteryPancake@users.noreply.github.com>
2022-09-19 23:04:21 +1000

    coin thing

---
+++ Group Project/Assets/Scripts/CoinText.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinText : MonoBehaviour
+{
+    public int coins;
+
+    void Update() {
+        this.GetComponent<UnityEngine.UI.Text>().text = "Coins: " + coins;
+    }
+    public void add(int i) {
+        coins = coins + i;
+        GetComponent<AudioSource>().Play();
+    }
+}
