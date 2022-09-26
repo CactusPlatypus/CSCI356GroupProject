8c656a715b11948a76994f58675a3a14359eb214
CactusPlatypus <outcascade@gmail.com>
2022-09-26 13:24:16 +1000

    wine warp added. Messes with pause menu slightly

---
+++ Group Project/Assets/Scripts/Pickups/WinePickup.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinePickup : MonoBehaviour
+{
+    [SerializeField] private ScoreManager scoreController;
+    private const float rotationSpeed = 100f;
+    [SerializeField] private float speedMultiplier = 1.5f;
+    [SerializeField] private float time = 9.0f;
+    [SerializeField] private GameObject wineWarp;
+
+    private void Start()
+    {
+        // Random initial coin rotation
+        transform.Rotate(Vector3.up, Random.Range(0f, 360f));
+        scoreController = GameObject.Find("ScoreManager").GetComponent<ScoreManager>();
+    }
+
+    private void Update()
+    {
+        // Make coin spin for fun
+        transform.Rotate(Vector3.up, Time.deltaTime * rotationSpeed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+
+            scoreController.powerUpPopUp("Wine Warp!", time - 1);
+            Instantiate(wineWarp);
+
+
+            Destroy(gameObject);
+        }
+    }
+}
