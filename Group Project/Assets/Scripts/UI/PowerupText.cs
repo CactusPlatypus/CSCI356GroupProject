c984ca91ccf6f2d0e5de883ef8c926619941cfe4
MysteryPancake <MysteryPancake@users.noreply.github.com>
2022-10-19 19:23:25 +1100

    move poweruptext to game ui

--- Group Project/Assets/Scripts/UI/PowerupText.cs
+++ Group Project/Assets/Scripts/UI/PowerupText.cs
@@ -8,14 +8,12 @@ public class PowerupText : MonoBehaviour
     public LeanTweenType easeIn;
     public LeanTweenType easeOut;
 
-    private GameObject powerupText;
     private AudioSource sound;
     private Coroutine timer;
     private const float hideTime = 2f;
 
     private void Start()
     {
-        powerupText = transform.GetChild(0).gameObject;
         sound = GetComponent<AudioSource>();
     }
 
@@ -25,12 +23,12 @@ public class PowerupText : MonoBehaviour
         if (timer != null) StopCoroutine(timer);
 
         // Animate manually
-        powerupText.transform.localScale = new Vector3(0f, 0f, 0f);
-        LeanTween.scale(powerupText, new Vector3(1f, 1f, 1f), 0.5f)
+        transform.localScale = new Vector3(0f, 0f, 0f);
+        LeanTween.scale(gameObject, new Vector3(1f, 1f, 1f), 0.5f)
             .setEase(easeIn)
             .setIgnoreTimeScale(true);
 
-        powerupText.GetComponent<TMP_Text>().text = text;
+        gameObject.GetComponent<TMP_Text>().text = text;
         sound.Play();
         
         // Hide text after time
@@ -40,7 +38,7 @@ public class PowerupText : MonoBehaviour
     public IEnumerator CloseText()
     {
         yield return new WaitForSeconds(hideTime);
-        LeanTween.scale(powerupText, new Vector3(0f, 0f, 0f), 0.5f)
+        LeanTween.scale(gameObject, new Vector3(0f, 0f, 0f), 0.5f)
             .setEase(easeOut)
             .setIgnoreTimeScale(true);
     }
