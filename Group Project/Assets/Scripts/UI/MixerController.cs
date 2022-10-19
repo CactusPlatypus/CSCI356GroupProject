f16fe2ca749fd81e50a733b84fb2161ca296fec1
MysteryPancake <MysteryPancake@users.noreply.github.com>
2022-10-19 16:12:54 +1100

    remove test assets, save volume settings

---
+++ Group Project/Assets/Scripts/UI/MixerController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class MixerController : MonoBehaviour
+{
+    [SerializeField] private AudioMixer audioMixer;
+
+    [SerializeField] private Slider masterSlider;
+    [SerializeField] private Slider musicSlider;
+    [SerializeField] private Slider SFXSlider;
+
+    private void Start()
+    {
+        float masterVolume = PlayerPrefs.GetFloat("MasterVolume");
+        float musicVolume = PlayerPrefs.GetFloat("MusicVolume");
+        float SFXVolume = PlayerPrefs.GetFloat("SFXVolume");
+
+        // Update sliders, causes mixer to update too
+        masterSlider.value = masterVolume;
+        musicSlider.value = musicVolume;
+        SFXSlider.value = SFXVolume;
+    }
+
+    public void setMasterVolume(float volume)
+    {
+        PlayerPrefs.SetFloat("MasterVolume", volume);
+        audioMixer.SetFloat("MasterVolume", volume);
+    }
+
+    public void setMusicVolume(float volume)
+    {
+        PlayerPrefs.SetFloat("MusicVolume", volume);
+        audioMixer.SetFloat("MusicVolume", volume);
+    }
+
+    public void setSFXVolume(float volume)
+    {
+        PlayerPrefs.SetFloat("SFXVolume", volume);
+        audioMixer.SetFloat("SFXVolume", volume);
+    }
+}
--- Group Project/Assets/Scripts/UI/SettingsMenu.cs
+++ Group Project/Assets/Scripts/UI/SettingsMenu.cs
@@ -1,51 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Audio;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI;
 
 public class SettingsMenu : MonoBehaviour
 {
-    public AudioMixer audioMixer;
-
     // Menu displaying settings button, either pause menu or main menu
     public GameObject hostMenu;
-
-    [SerializeField] private Slider mainSlider;
-    [SerializeField] private Slider musicSlider;
-    [SerializeField] private Slider SFXSlider;
-
-    public void Start()
-    {
-        updateSliders();
-    }
-
-    public void updateSliders()
-    {
-        float val;
-        audioMixer.GetFloat("MasterVolume", out val);
-        mainSlider.value = val;
-        audioMixer.GetFloat("MusicVolume", out val);
-        musicSlider.value = val;
-        audioMixer.GetFloat("SFXVolume", out val);
-        SFXSlider.value = val;
-    }
-
-    public void setMasterVolume(float volume)
-    {
-        audioMixer.SetFloat("MasterVolume", volume);
-    }
-
-    public void setMusicVolume(float volume)
-    {
-        audioMixer.SetFloat("MusicVolume", volume);
-    }
-
-    public void setSFXVolume(float volume)
-    {
-        audioMixer.SetFloat("SFXVolume", volume);
-    }
 
     public void showSettings()
     {
